7126b756a68efa82a60e5631d276e9ed9d7d815a
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-02-15 18:18:44 -0400

    Proyecto

---
+++ Registro3.5/UI/Pago/Pagos.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Registro3._5.UI.Pago
+{
+    /// <summary>
+    /// Interaction logic for Pagos.xaml
+    /// </summary>
+    public partial class Pagos : Window
+    {
+        public Pagos()
+        {
+            InitializeComponent();
+        }
+    }
+}
